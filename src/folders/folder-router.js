35e1eb74592645b8e2534bcca5f7be3e3193bb31
Logan Ellis <mrloganellis@gmail.com>
2020-03-04 13:32:02 -0500

    fixed folders enpoint

--- src/folders/folder-router.js
+++ src/folders/folder-router.js
@@ -9,7 +9,7 @@ const jsonParser = express.json()
 // pull each piece of logic out of each route, all after .get, and put that all into a route file with a function for each route.
 folderRouter
 
-  .route( '/folder' )
+  .route( '/folders' )
 
   .get( ( req, res, next ) => {
     const knexInstance = req.app.get( 'db' )
@@ -38,7 +38,7 @@ folderRouter
 
 folderRouter
 
-  .route( '/folder/:folderId' )
+  .route( '/folders/:folderId' )
 
   .all( ( req, res, next ) => {
     FolderService.getFolderById(
