9c1d111bb9337d41de02f89f7263f5d46699e5e7
Logan Ellis <mrloganellis@gmail.com>
2020-03-04 13:14:05 -0500

    redownloaded cors

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     PORT : process.env.PORT || 8000,
     NODE_ENV : process.env.NODE_ENV,
-    API_KEY : process.env.API_KEY,
     DB_URL : process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
     TEST_DB_URL : process.env.TEST_DATABASE_URL
   }
